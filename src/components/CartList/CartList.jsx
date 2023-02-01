3dc760f7ad0c31cdeb359a695c86f36b19dc296c
Seng Sopheng <phengg1405@gmail.com>
2023-02-01 16:32:57 +0700

    edit cart1

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -86,69 +86,31 @@ const CartList = () =>{
             
         </div>
         <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-lg-12">
+            <div className="row">
+                <div className="col-lg-4">
                     <div className="card">
-                        <div className="row">
-                            <div className="col-lg-3 radio-group">
-                                <div className="row d-flex px-3 radio">
-                                    <img className="pay" src="https://i.imgur.com/WIAP9Ku.jpg"/>
-                                    <p className="my-auto">Credit Card</p>
-                                </div>
-                                <div className="row d-flex px-3 radio gray">
-                                    <img className="pay" src="https://i.imgur.com/OdxcctP.jpg"/>
-                                    <p className="my-auto">Debit Card</p>
-                                </div>
-                                <div className="row d-flex px-3 radio gray mb-3">
-                                    <img className="pay" src="https://i.imgur.com/cMk1MtK.jpg"/>
-                                    <p className="my-auto">PayPal</p>
-                                </div>
-                            </div>
-                            <div className="col-lg-5">
-                                <div className="row px-2">
-                                    <div className="form-group col-md-6">
-                                        <label className="form-control-label">Name on Card</label>
-                                        <input type="text" id="cname" name="cname" placeholder="Johnny Doe"/>
+                        <div className="col-lg-12 mt-2">
+                                    <div className="row d-flex justify-content-between px-4">
+                                        <p className="mb-1 text-left">Cart</p>
+                                        {
+                                            cart.cartItems.map(e=><p className="fw-light" key={e.id}>- {e.title.substring(0,35)}...</p>)
+                                        }
                                     </div>
-                                    <div className="form-group col-md-6">
-                                        <label className="form-control-label">Card Number</label>
-                                        <input type="text" id="cnum" name="cnum" placeholder="1111 2222 3333 4444"/>
-                                    </div>
-                                </div>
-                                <div className="row px-2">
-                                    <div className="form-group col-md-6">
-                                        <label className="form-control-label">Expiration Date</label>
-                                        <input type="text" id="exp" name="exp" placeholder="MM/YYYY"/>
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label className="form-control-label">CVV</label>
-                                        <input type="text" id="cvv" name="cvv" placeholder="***"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 mt-2">
-                                <div className="row d-flex justify-content-between px-4">
-                                    <p className="mb-1 text-left">Cart</p>
-                                    {
-                                        cart.cartItems.map(e=><p className="fw-light" key={e.id}>- {e.title.substring(0,35)}...</p>)
-                                    }
-                                </div>
-                                
-                                <div className="row d-flex justify-content-between px-4" id="tax">
-                                    <p className="mb-1 text-left">Total (tax included)</p>
+                                    
+                                    <div className="row d-flex justify-content-between px-4" id="tax">
+                                        <p className="mb-1 text-left">Total (tax included)</p>
 
-                                    <h6 className="mb-1 text-right">{cart.cartTotalAmount}$</h6>
-                                </div>
-                                <div className="row d-flex justify-content-between px-4" id="tax">
-                                <button className="btn-block btn-orange ">
-                                    <span>
-                                        <span id="btn bg-orange">Checkout </span>
-                                        
-                                    </span>
-                                </button>
-                                </div>
-                                
-                            </div>
+                                        <h6 className="mb-1 text-right">{cart.cartTotalAmount}$</h6>
+                                    </div>
+                                    <div className="row d-flex justify-content-between px-4" id="tax">
+                                    <button className="btn-block btn-orange ">
+                                        <span>
+                                            <span id="btn bg-orange">Checkout </span>
+                                            
+                                        </span>
+                                    </button>
+                                    </div>
+                                    
                         </div>
                     </div>
                 </div>
